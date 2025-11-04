44d5708f5278ac54120df463b39230b3d083cb39
Pablo Araya <pabloaraya@192.168.1.87>
2025-11-04 18:24:23 -0300

    optimizacion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
         <div className={`hero-video ${isVideoPlaying ? 'video-playing' : 'video-thumbnail-mode'}`}>
           {!isVideoPlaying ? (
             <div className="video-thumbnail" onClick={() => setIsVideoPlaying(true)}>
-              <img src={videoThumbnail} alt="Video preview" />
+              <img src={videoThumbnail} alt="Video preview" fetchPriority="high" />
               <div className="play-button">
                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M8 5.14v13.72L19 12L8 5.14z" fill="#131415"/>
